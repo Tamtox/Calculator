c7df444a8e94c6cb9c5fc269d9e3f956a637ddb2
Tamtox <39272747@protonmail.com>
2020-07-06 13:35:20 +0600

    js

--- Calc.js
+++ Calc.js
@@ -1,9 +1,4 @@
 const calculation = document.querySelector('#calculations');
-//Other inputs
-const clear = document.querySelector('#clear')
-clear.addEventListener('click',()=>{calculation.innerHTML=""})
-const del = document.querySelector('#del')
-del.addEventListener('click',()=>{calculation.innerHTML=calculation.innerHTML.slice(0,-1)})
 //Numbers
 const arrNums = 'one two three four five six seven eight nine decimal zero percent'.split(" ")
 const nums = {
@@ -13,6 +8,18 @@ for(let i=0;i<arrNums.length;i++) {
     nums[arrNums[i]]=document.querySelector(`#${arrNums[i]}`)
 }
 for(let i in nums) {
-    numsEvents.push(nums[i].addEventListener('click',()=>{console.log(nums[i].innerHTML)}))
+    numsEvents.push(nums[i].addEventListener('click',()=>{calculation.innerHTML+=nums[i].innerHTML}))
 }
 //Operators
+const clear = document.querySelector('#clear')
+clear.addEventListener('click',()=>{calculation.innerHTML=""})
+const del = document.querySelector('#del')
+del.addEventListener('click',()=>{calculation.innerHTML=calculation.innerHTML.slice(0,-1)})
+const plus = document.querySelector('#plus')
+plus.addEventListener('click',()=>{calculation.innerHTML+="+"})
+const minus = document.querySelector('#minus')
+minus.addEventListener('click',()=>{calculation.innerHTML+="-"})
+const multiply = document.querySelector('#multiply')
+multiply.addEventListener('click',()=>{calculation.innerHTML+="*"})
+const divide = document.querySelector('#divide')
+divide.addEventListener('click',()=>{calculation.innerHTML+="/"})
