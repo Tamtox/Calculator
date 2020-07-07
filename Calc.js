4154193d4ceae002981678190e446dd66f7e3f49
Tamtox <39272747@protonmail.com>
2020-07-07 12:08:41 +0600

    only calculation left

--- Calc.js
+++ Calc.js
@@ -1,6 +1,15 @@
 const calculation = document.querySelector('#calculations');
+//Check for repeating operators
+function check() {
+    let operators = "+-*/.".split("");
+    for(let i of operators) {
+        if(operators.includes(calculation.innerHTML[calculation.innerHTML.length-1]) && operators.includes(calculation.innerHTML[calculation.innerHTML.length-2])) {
+            calculation.innerHTML=calculation.innerHTML.slice(0,-1)
+        }
+    }
+}
 //Numbers
-const arrNums = 'one two three four five six seven eight nine decimal zero percent'.split(" ")
+const arrNums = 'one two three four five six seven eight nine zero'.split(" ")
 const nums = {
 }
 const numsEvents = [];
@@ -8,7 +17,8 @@ for(let i=0;i<arrNums.length;i++) {
     nums[arrNums[i]]=document.querySelector(`#${arrNums[i]}`)
 }
 for(let i in nums) {
-    numsEvents.push(nums[i].addEventListener('click',()=>{calculation.innerHTML+=nums[i].innerHTML}))
+    numsEvents.push(nums[i].addEventListener('click',()=>{
+        calculation.innerHTML+=nums[i].innerHTML}))
 }
 //Operators
 const clear = document.querySelector('#clear')
@@ -16,17 +26,52 @@ clear.addEventListener('click',()=>{calculation.innerHTML=""})
 const del = document.querySelector('#del')
 del.addEventListener('click',()=>{calculation.innerHTML=calculation.innerHTML.slice(0,-1)})
 const plus = document.querySelector('#plus')
-plus.addEventListener('click',()=>{calculation.innerHTML+="+"})
+plus.addEventListener('click',()=>{
+    if(calculation.innerHTML.length>0) {
+        calculation.innerHTML+="+"
+    }
+    check()
+})
 const minus = document.querySelector('#minus')
-minus.addEventListener('click',()=>{calculation.innerHTML+="-"})
+minus.addEventListener('click',()=>{
+    if(calculation.innerHTML.length>0) {
+        calculation.innerHTML+="-"
+    }
+    check()
+})
 const multiply = document.querySelector('#multiply')
-multiply.addEventListener('click',()=>{calculation.innerHTML+="*"})
+multiply.addEventListener('click',()=>{
+    if(calculation.innerHTML.length>0) {
+        calculation.innerHTML+="*"
+    }
+    check()
+})
 const divide = document.querySelector('#divide')
-divide.addEventListener('click',()=>{calculation.innerHTML+="/"})
+divide.addEventListener('click',()=>{
+    if(calculation.innerHTML.length>0) {
+        calculation.innerHTML+="/"
+    }
+    check()
+})
+const decimal = document.querySelector('#decimal');
+decimal.addEventListener('click',()=>{
+    if(calculation.innerHTML.length>0) {
+        calculation.innerHTML+="."
+    }
+    check()
+})
+const percent = document.querySelector('#percent');
+percent.addEventListener('click',()=>{
+    const digits = '1234567890';
+    if(digits.includes(calculation.innerHTML[calculation.innerHTML.length-1]) && calculation.innerHTML.length>0) {
+        calculation.innerHTML+="%"
+    }
+    check()
+})
 //Calculator Function
 function calculate() {
-    let x = calculation.innerHTML
-    
+    let decimal = /\d+\W\d+/g;
+    let int = /\d+/g;
 }
 let equals = document.querySelector('#equals');
 equals.addEventListener('click',()=>{
